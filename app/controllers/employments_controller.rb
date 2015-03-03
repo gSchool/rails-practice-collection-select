bc2774a4bdeaa30d98d3468763d5a5fae792f607
David Abramowitz <david.xesllc@gmail.com>
2015-03-03 14:38:25 -0700

    Final w Bonus

--- app/controllers/employments_controller.rb
+++ app/controllers/employments_controller.rb
@@ -1,16 +1,32 @@
 class EmploymentsController < ApplicationController
+  helper_method :toggle_blank
+
+  def toggle_blank
+    if @toggle_blank
+      {include_blank: true}
+    else
+      {include_blank: false}
+    end
+  end
 
   def index
     @employments = Employment.all
+
   end
 
   def new
+    @toggle_blank = true
     @employment = Employment.new
+    @persons = Person.all
+    @organizations = Organization.all
+    @jobs = JobTitle.all
+    @locations = Location.all
   end
 
   def create
     @employment = Employment.new(employment_params)
     if @employment.save
+      @toggle_blank = false
       redirect_to Employment
     else
       render :new
@@ -18,7 +34,12 @@ class EmploymentsController < ApplicationController
   end
 
   def edit
+    @toggle_blank = false
     @employment = Employment.find(params[:id])
+    @persons = Person.all
+    @organizations = Organization.all
+    @jobs = JobTitle.all
+    @locations = Location.all
   end
 
   def update
